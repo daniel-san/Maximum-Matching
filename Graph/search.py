d7aa9d768353a5448064ea2340fa7f657cf5d13e
daniel-san <dlsbdaniel@gmail.com>
2015-04-30 17:35:35 -0300

    Graph search algorithms drafts added

---
+++ Graph/search.py
@@ -0,0 +1,13 @@
+#Graph Search Algorithms
+
+def dfs():
+    """
+    Iterative Depth-First Search implementation
+    """
+    pass
+
+def bfs():
+    """
+    Breadth-First Search implementation
+    """
+    pass
