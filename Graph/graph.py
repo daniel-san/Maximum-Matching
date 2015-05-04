340adb101af7a61ae1f752caafc20de9b9eb5863
daniel-san <dlsbdaniel@gmail.com>
2015-05-04 20:28:10 -0300

    Some work on DFS and BFS

--- Graph/graph.py
+++ Graph/graph.py
@@ -1,31 +1,33 @@
 #Graph classes draft
 
 class Node():
-    def __init__(self,x,y):
-        self.group = -1
-        self.X = x
-        self.Y = y
+    def __init__(self,x,y,id):
+        self.id = -1
+        self.status = None #Status, used in algorithms like DFS
+        self.X = x #X coordinate, if used
+        self.Y = y #Y coordinate, if used
+        self.adj = [] #List of adjacent vertices
     
-    
-class Edge():
-    def __init__(self,n1,n2):
-        self.n1 = n1
-        self.n2 = n2
-        self.weight = None
-        
-    def calcWeight(self):
-        self.weight = math.sqrt((self.n2.X - self.n1.X)**2 + (self.n2.Y - self.n1.Y)**2)
-	
-    def __cmp__(self,other):
-        if self.weight < other.weight:
-            return -1
-        elif self.weight > other.weight:
-            return 1
-        else:
-            return 0
-	
+#Designing graph without a Edge class for now...    
+#class Edge():
+#    def __init__(self,n1,n2):
+#        self.n1 = n1
+#        self.n2 = n2
+#        self.weight = None
+#        
+#    def calcWeight(self):
+#        self.weight = math.sqrt((self.n2.X - self.n1.X)**2 + (self.n2.Y - self.n1.Y)**2)
+#	
+#    def __cmp__(self,other):
+#        if self.weight < other.weight:
+#            return -1
+#        elif self.weight > other.weight:
+#            return 1
+#        else:
+#            return 0
+
 class Graph():
     def __init__(self):
-        self.nodes = []
-        self.edges = []
+        self.nodes = [] #List of vertices
+        self.edges = [] #List of edges. A dictionary can be used in case of Weighted Edges
         
--- Graph/search.py
+++ Graph/search.py
@@ -1,13 +1,30 @@
 #Graph Search Algorithms
 
-def dfs():
+def dfs(G, v):
     """
-    Iterative Depth-First Search implementation
+    Iterative Depth-First Search base implementation
     """
-    pass
+    s = []
+    s.append(v)
+    while s:
+        v = s.pop()
+        if v.status != "visited":
+            v.status = "visited"
+            for i in v.adj:
+                s.append(i)
+
+    
 
 def bfs():
     """
-    Breadth-First Search implementation
+    Breadth-First Search base implementation
     """
-    pass
+    q = []
+    q.append(v) #Enqueue
+    while q:
+        v = q.pop(0) #Dequeue
+        for i in v.adj:
+            if i.status != "visited":
+                q.append(i)
+                i.status = "visited"
+        
