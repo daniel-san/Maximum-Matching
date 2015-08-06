a806d275a04086dde8ab183288a4ea53649bae33
daniel-san <dlsbdaniel@gmail.com>
2015-08-06 15:12:56 -0300

    Adding implementation of BFS and DFS, as well as a linked list struct type and functions  for handling vertices neighbors

--- Graph/C/search.c
+++ Graph/C/search.c
@@ -12,14 +12,14 @@ void DFS(Graph g, Vertex v)
         v = (Vertex) stack_pop(stack)->data;
         if (!v.visited)
         {
-			v.visited = 1;
-			neighbors = v.neighbors;
-			temp = neighbors->head;
-			while (temp != NULL)
-			{
-				stack_push(stack, temp->data);
-				temp = temp->next;
-			}
+            v.visited = 1;
+            neighbors = v.neighbors;
+            temp = neighbors->head;
+            while (temp != NULL)
+            {
+                stack_push(stack, temp->data);
+                temp = temp->next;
+            }
 
         }
     }
@@ -27,26 +27,26 @@ void DFS(Graph g, Vertex v)
 
 void BFS(Graph g, Vertex v)
 {
-	Queue* queue = queue_create();
-	List* neighbors;
-	Element* temp;
+    Queue* queue = queue_create();
+    List* neighbors;
+    Element* temp;
 
-	queue_enqueue(queue, (void*)v);
-	while(queue->queue_size > 0)
-	{
-		v = (Vertex) queue_dequeue(queue)->data;
-		neighbors = v.neighbors;
-		temp = neighbors->head;
-		while (temp != NULL)
-		{
-			v = (Vertex) temp->data;
-			if (!v.visited)
-			{
-				v.visited = 1;
-				queue_enqueue(queue, (void*) v);
+    queue_enqueue(queue, (void*)v);
+    while(queue->queue_size > 0)
+    {
+        v = (Vertex) queue_dequeue(queue)->data;
+        neighbors = v.neighbors;
+        temp = neighbors->head;
+        while (temp != NULL)
+        {
+            v = (Vertex) temp->data;
+            if (!v.visited)
+            {
+                v.visited = 1;
+                queue_enqueue(queue, (void*) v);
 
-			}
-			temp = temp->next;
-		}
-	}
+            }
+            temp = temp->next;
+        }
+    }
 }       
