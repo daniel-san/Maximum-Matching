00e9c7089111e415f54539fa874bbd3d40f9e85e
daniel-san <dlsbdaniel@gmail.com>
2015-08-24 09:50:56 -0300

    Adding graph generating functions

---
+++ Graph/C/graph_generator.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "graph_generator.h"
+
+Graph
+random_graph (size_t n, size_t m)
+{
+
+}
+
+Graph
+scale_free_graph (size_t n, size_t m)
+{
+
+}
+
+Graph
+small_world_graph (size_t n, size_t m)
+{
+
+}
---
+++ Graph/C/graph_generator.h
@@ -0,0 +1,5 @@
+#include "graph.h"
+
+Graph random_graph (size_t n, size_t m);
+Graph scale_free_graph (size_t n, size_t m);
+Graph small_world_graph (size_t n, size_t m);
